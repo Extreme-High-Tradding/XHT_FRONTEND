a1b664f934ab0fa18d7d355bca029c1612e56417
CamiloASuarez <camiloandressuarez1@gmail.com>
2020-08-21 12:58:30 -0500

    server conf

--- server.js
+++ server.js
@@ -2,10 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = "3000";
-//const publicpath = path.join(__dirname, '..', 'public');
+
 
 app.use(express.static(path.join(__dirname, '/dist')));
-
 
 app.listen(port, () => {
   console.log('Server is running on ' + port);
