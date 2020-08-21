45e6e372bf421ca565d84a9b33a8ccc2e1f4d42e
CamiloASuarez <camiloandressuarez1@gmail.com>
2020-08-21 17:40:10 -0500

    add sass loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 
 const App = ()=>(
   <div className="app">Welcome to XHT
   <div className="title">HELLO IM A TITLEs</div>
+  <h1>PROBANDO SASS</h1>
   </div>
 )
 
