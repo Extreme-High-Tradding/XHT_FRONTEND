cac8cd572a2084214a43ea70350822ab273f50c7
devcabezas <luiscabezas25@hotmail.com>
2020-08-21 18:10:27 -0500

    webpack images

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.scss';
+import logo from './assets/static/logo_header.png';
 
 
 const App = ()=>(
   <div className="app">Welcome to XHT
   <div className="title">HELLO IM A TITLEs</div>
+  <img className="header__logo" src={logo} alt="logo" />
   <h1>PROBANDO SASS</h1>
   </div>
 )
