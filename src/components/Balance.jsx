c3cbe5258bc1e62f8ef6f947aebc0adea908f5c3
devcabezas <luiscabezas25@hotmail.com>
2020-08-21 20:54:29 -0500

    component file creation

---
+++ src/components/Balance.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Balance = () => (
+  <h1>Balance</h1>
+);
+
+export default Balance;
---
+++ src/components/RecentsTrades.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const RecentsTrades = () => (
+  <h1>RecentsTrades
+</h1>
+);
+
+export default RecentsTrades;
