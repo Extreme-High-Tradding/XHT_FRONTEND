ae0aae325a0107ab01195682129a8645c14b7fb9
devcabezas <luiscabezas25@hotmail.com>
2020-08-23 21:47:30 -0500

    upgrade function buy and sell

--- src/components/BuyandSell/BuyandSell.jsx
+++ src/components/BuyandSell/BuyandSell.jsx
@@ -1,21 +1,67 @@
-import React from 'react';
+import React, { Component} from 'react';
 import Sell from './Sell';
 import Buy from './Buy';
 import '../../App.scss';
 
-const BuyandSell = () => {
+class BuyandSell extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isToggleOn: true
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn
+    }));
+  }
+
+  render() {
+    if (this.state.isToggleOn) {
+      return (
+        <React.Fragment>
+          <div className="order__container">
+            <ul className="order__btn">
+              <li><button disabled onClick={this.handleClick} className="order1__buy">Buy</button></li>
+              <li><button onClick={this.handleClick} className="order__sell">Sell</button></li>
+            </ul>
+            <Buy />
+          </div>
+        </React.Fragment>
+      )
+    } else {
+      return (
+        <React.Fragment>
+          <div className="order__container">
+            <ul className="order__btn">
+              <li><button onClick={this.handleClick} className="order1__buy">Buy</button></li>
+              <li><button disabled onClick={this.handleClick} className="order__sell">Sell</button></li>
+            </ul>
+            <Sell />
+          </div>
+        </React.Fragment>
+      )
+    }
+  }
+
+}
+
+
+/* const BuyandSell = () => {
+  
   return (
     <React.Fragment>
       <div className="order__container">
         <ul className="order__btn">
-          <li><a hrer="#">Buy</a></li>
-          <li><a hrer="#">Sell</a></li>
+          <li><a href="/sell">Buy</a></li>
+          <li><a href="/buy">Sell</a></li>
         </ul>
-        <Sell />
-        <Buy />
+        
       </div>
     </React.Fragment>
   )
-}
+} */
 
 export default BuyandSell;
