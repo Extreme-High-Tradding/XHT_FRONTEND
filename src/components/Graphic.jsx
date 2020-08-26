a1402c6b84f28a9c46ee182d584ed91c59e5b710
devcabezas <luiscabezas25@hotmail.com>
2020-08-26 06:24:56 -0500

    TradingView

--- src/components/Graphic.jsx
+++ src/components/Graphic.jsx
@@ -1,61 +1,10 @@
 import React from 'react';
+import TradingViewWidget, { Themes } from 'react-tradingview-widget';
 
-
-class Graphic extends React.Component {
-  /* componentDidMount () {
-    const script = document.createElement("script");
-
-    script.src = "https://s3.tradingview.com/tv.js";
-    script.async = true;
-
-    document.body.appendChild(script);
-  } */
-
-  render() {
-    function LoadCharts() {
-     /*  let symbol = document.getElementById('pairs').value; */
-      new TradingView.widget(
-      {
-      "width": 980,
-      "height": 610,
-      "symbol": "BINANCE:BTCUSDT",
-      "interval": "D",
-      "timezone": "Etc/UTC",
-      "theme": "dark",
-      "style": "1",
-      "locale": "in",
-      "toolbar_bg": "#f1f3f6",
-      "enable_publishing": false,
-      "allow_symbol_change": true,
-      "container_id": "tvchart"
-      })
-      }
-     /*  LoadCharts(); */
-    return (
-      <div>
-        <select onchange={LoadCharts()} id="pairs">
-            <option value="BTC/USDT">BTC/USDT</option>
-            {/* <option value="BTC/USDT">ETH/USDT</option> */}
-        </select>
-        <div class="tradingview-widget-container" id="tvchart">
-        <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
-        </div>
-      </div>
-    )
-  }
-}
-
-/* const Graphic = () => (
-    <div class="tradingview-widget-container">
-      <div id="tradingview_70c14"></div>
-      <div class="tradingview-widget-copyright"><a href="https://in.tradingview.com/symbols/NASDAQ-AAPL/" rel="noopener" target="_blank"><span class="blue-text">AAPL Chart</span></a> by TradingView</div>
-      <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
-      <script type="text/javascript">
-      
-          )};
-      </script>
-    </div>
-
-); */
+const Graphic = () => (
+  <TradingViewWidget symbol="NASDAQ:TSLA" /*theme={Themes.DARK}*/
+  locale="fr"
+  autosize />
+);
 
 export default Graphic;
