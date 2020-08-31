50347e4906235ea7655f64cad951e217730fcb92
devcabezas <luiscabezas25@hotmail.com>
2020-08-30 19:02:16 -0500

    change Two

--- src/components/Markets.jsx
+++ src/components/Markets.jsx
@@ -14,9 +14,9 @@ class Markets extends Component {
     axios.get('https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,BCH&tsyms=USD&e=Coinbase&extraParams=your_app_name')
       .then(res => {
         const cryptos = res.data;
-        /* console.log(cryptos); */
+        console.log(cryptos);
         this.setState({cryptos: cryptos.RAW})
-        /* console.log('Pruebo Objetos', (this.state.cryptos)); */
+        console.log('Pruebo Objetos', (this.state.cryptos));
       })
     }
 
