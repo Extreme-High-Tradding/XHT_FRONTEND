144d929f181978f90c489331926dd18e08be30c4
CamiloASuarez <camiloandressuarez1@gmail.com>
2020-08-30 19:29:23 -0500

    server fix

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 
-app.use(express.static(path.join(__dirname, '/dist')));
+app.use(express.static(__dirname + '/public'));
+app.get('/', function (req, res) {
+  res.render('index');
+})
 
 app.listen(port, () => {
   console.log('Server is running on ' + port);
