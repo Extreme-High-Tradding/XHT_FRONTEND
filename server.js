7b9f24f0253351d21d447639360c4f1a905e1139
CamiloASuarez <camiloandressuarez1@gmail.com>
2020-08-30 19:13:50 -0500

    add

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 
-app.use(express.static(__dirname, '/public'));
+app.use(express.static(path.join(__dirname, 'src')));
 
 app.listen(port, () => {
   console.log('Server is running on ' + port);
