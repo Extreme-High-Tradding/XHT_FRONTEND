9903f32a15bbed1c760c9f16c6e1915909a63e5f
CamiloASuarez <camiloandressuarez1@gmail.com>
2020-08-30 23:24:06 -0500

    reload

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 
-app.use(express.static(path.join(__dirname + '/public')));
+app.use(express.static(path.join(__dirname + '/dist')));
 
 app.listen(port, () => {
   console.log('Server is running on ' + port);
